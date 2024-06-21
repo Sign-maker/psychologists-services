b52349d1fac2188173793ef3ff8879c87193b3ca
Sign-maker <Sign-maker@users.noreply.github.com>
2024-06-21 22:43:41 +0000

    deploy: 879d389b90b8760379b13a0239b7c7b5de87f995

--- assets/Favorites-CV14ymW4.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-CIPNu5f8.js";const s=()=>r.jsx("div",{children:"Favorites"});export{s as default};
--- assets/Psychologists-DfET33L8.js
+++
@@ -1,1 +0,0 @@
-import{j as s}from"./index-CIPNu5f8.js";const t=()=>s.jsx("div",{children:"Psychologists"});export{t as default};
